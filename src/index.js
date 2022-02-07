45320f11355cc6c0fe6bf78c89a2c54458cdd17c
Klementeva Katya <87204318+easy-breezy-lemon-squeezy@users.noreply.github.com>
2022-02-07 18:29:51 +0300

    Исправлен метатег для адаптивности

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
-import MetaTags from 'react-meta-tags';
 
 ReactDOM.render(
         <Provider store={store}>
-            <MetaTags>
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-            </MetaTags>
             <App/>
         </Provider>,
     document.getElementById('root')
